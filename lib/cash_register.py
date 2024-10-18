7e91e26e2ff24d6f1250555d95419c7366dd380c
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-18 09:56:49 +0300

    commit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,22 +1,22 @@
 class CashRegister:
     def __init__(self, discount=0):
-        self.total = 0
-        self.discount = discount  # Accept discount during initialization
-        self.last_transaction = 0
+        self.total = 0.0
+        self.items = []
+        self.discount = discount
 
-    def add_item(self, price, quantity=1):
+    def add_item(self, title, price, quantity=1):
         self.last_transaction = price * quantity
         self.total += self.last_transaction
+        self.items.append((title, price, quantity))
 
     def apply_discount(self):
         if self.discount > 0:
-            discount_amount = (self.total * self.discount / 100)
+            discount_amount = (self.total * self.discount) / 100
             self.total -= discount_amount
-            return f"After the discount, the total comes to ${self.total:.2f}."
+            print(f"After the discount, the total comes to ${self.total:.2f}.")
         else:
-            return "There is no discount to apply."
+            print("There is no discount to apply.")
 
     def void_last_transaction(self):
         self.total -= self.last_transaction
-        self.last_transaction = 0  # Reset last transaction after voiding
-
+       
